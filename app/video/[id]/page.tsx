bb03a42c73881172fefe000d4fbe4929dfe65f5a
김동우 <t6u80o@naver.com>
2024-10-07 23:22:42 +0900

    ruleset test

--- app/video/[id]/page.tsx
+++ app/video/[id]/page.tsx
@@ -15,6 +15,7 @@ export default function VideoDetail(props :PropsType){
             <div className="col-md-3 col-lg-2 p-0 d-none d-md-block">
                 <SideBarContainer />
             </div>
+            ㅎㅎ
             <div className="col-md-9 col-lg-10 col-12">
                 <MainContainer videoId={videoId} />
             </div>

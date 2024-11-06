1bfb5d77bc3cb4746fd21243caab2f9d693712fe
김동우 <t6u80o@naver.com>
2024-11-06 15:28:23 +0900

    로딩 스피너 색 변경

--- app/video/[id]/components/Comment/TopLikeCountContainer.tsx
+++ app/video/[id]/components/Comment/TopLikeCountContainer.tsx
@@ -68,7 +68,7 @@ export default function TopLikeContainer(
             ))}
             {
                 visibleData.length === commentData.length &&
-                <p className='m-0 text-center'>마지막 댓글입니다.</p>
+                <p className='m-0 fw-bold text-center'>마지막 댓글입니다.</p>
             }
             {/* 이 div가 스크롤 끝에 도달할 때마다 observer가 동작 */}
             <div ref={observerRef} style={{ height: "1px" }}></div>

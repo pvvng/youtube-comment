1698dea952e789e321277d6b01adfc8c048fbc91
김동우 <t6u80o@naver.com>
2024-11-18 20:23:54 +0900

    flask post api route 보안 작업 완료

--- app/components/Error/ErrorContainer.tsx
+++ app/components/Error/ErrorContainer.tsx
@@ -2,9 +2,13 @@ export default function ErrorContainer(
     {errorMessage} : {errorMessage : string|null}
 ){
     return(
-        <div className="w-100 text-center mb-3">
+        <div className="mt-2 w-100 text-center mb-3">
             <img src="/logo/logo-mask.png" width="50%" style={{maxWidth : 360}} />
-            <p className="m-0 fw-bold mt-3">이런! {errorMessage}</p>
+            <p className="m-0 fw-bold mt-3">
+                {errorMessage} 
+                <br className="m-0" />
+                에러가 발생했어요.
+            </p>
         </div>
     )
 }

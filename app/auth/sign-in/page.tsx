fbc9c2a9c4368c1a29a9f9a913e44f33b34a2e1b
김동우 <t6u80o@naver.com>
2024-11-20 20:01:29 +0900

    animate.css 삭제. large logo css 추가 및 img 최적화 작업

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -5,6 +5,7 @@ import '@/app/css/sign.css';
 import { Suspense } from 'react';
 import { GoogleSignInBtn, LoginAnotherAccountBtn } from "@/app/components/SignItems/SignBtn";
 import GoToMainBtn from './component/GoToMainBtn';
+import Image from 'next/image';
 
 const BACKGROUND_STYE = {
     width: '100%',
@@ -17,7 +18,16 @@ export default function SignInPage() {
     return (
         <div style={{ ...BACKGROUND_STYE}}>
             <div className="text-center" style={{margin : 'auto'}}>
-                <img src='/logo/logo-full.png' className='mb-3' width="100%" style={{maxWidth : 360}}/>
+                <div className='large-logo-container mb-3'>
+                    <Image 
+                        src='/logo/logo-full.png' 
+                        width={360}
+                        height={360}
+                        layout='responsive'
+                        alt='YoutuView Logo'
+                        priority
+                    />
+                </div>
                 <Suspense fallback={<p>loading...</p>} >
                     <GoogleSignInBtn />
                 </Suspense>
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,24 @@
-import 'animate.css';
 import '@/app/css/landing.css';
 import LadingSearchButton from './components/Landing/LandingSearchButton';
 import ContentContainer from './components/Landing/Content/ContentContainer';
 import GoMainBtn from './components/Landing/Content/GoMainBtn';
+import Image from 'next/image';
 
 export default function Home() {
     
     return (
         <div className='text-center' style={{overflow : 'hidden'}}>
-            <div className='mt-3 animate__animated animate__fadeIn'>
-                <img 
-                    src='logo/logo-full.png' 
-                    width="80%" 
-                    alt='YoutuView LOGO' 
-                    style={{maxWidth : 368}}
-                />
+            <div className='mt-3'>
+                <div className='large-logo-container mb-3'>
+                    <Image
+                        src='/logo/logo-full.png' 
+                        width={360}
+                        height={360}
+                        layout='responsive'
+                        alt='YoutuView Logo'
+                        priority
+                    />
+                </div>
                 <LadingSearchButton />
             </div>
             <GoMainBtn />

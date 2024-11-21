1c18e7b246a7762f99b4ce0049e1f2af8b5bde80
맥동우 <t6u80o@naver.com>
2024-11-21 23:35:01 +0900

    contact 페이지 작성

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+import NavbarContainer from "../components/Navbar/NavbarContainer";
+
+export default function ContactPage(){
+    return (
+        <>
+            <NavbarContainer />
+        </>
+    )
+}
